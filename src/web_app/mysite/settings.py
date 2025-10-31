d79d1520f33c59800613546929803861c5e9feae
soony0737@gmail.com <soony0737@gmail.com>
2025-10-31 19:56:32 +0200

    made the settings file use env variables

--- src/web_app/mysite/settings.py
+++ src/web_app/mysite/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -64,13 +66,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "Servitor_ninja",
-        "USER": "Omnissiah",
-        "PASSWORD": "bla",
-        "HOST": "postgres",
-        "PORT": "5432",
+        "NAME": os.getenv("DB_NAME", "Servitor_ninja"),
+        "USER": os.getenv("DB_USER", "Omnissiah"),
+        "PASSWORD": os.getenv("DB_PASSWORD", "bla"),
+        "HOST": os.getenv("DB_HOST", "postgres"),
+        "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
